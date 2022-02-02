6782a1280b435280bf31479d8c3c47ec5ce16294
Zhansaule Telisheva <jansaule97@mail.ru>
2022-02-02 20:32:11 +0500

    Declared variables and added some console.log

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+
+const title = "JS course: Lesson 02";
+let screens = "Simple, Complex, Interactive";
+
+let screenPrice = 1000;
+let rollback = 100;
+let fullPrice = 12000;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов "+" "+screenPrice+" "+"рублей");
+console.log("Стоимость разработки сайта "+fullPrice+" рублей");
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice*(rollback/100));
