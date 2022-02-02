12d5e55627ba78746b402dce7c43ccd071a36c54
Zhansaule Telisheva <jansaule97@mail.ru>
2022-02-02 11:21:07 +0500

    Added new variables, console.log and alert message

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const title = "My first JS lesson";
+let screens = [13, 15, 17];
+let screenPrice = 2000;
+let roolback = 100;
+let fullPrice = 2100;
+let adaptive = "true";
+
 console.log("Hello world!");
+alert("My very first lesson in JS course");
